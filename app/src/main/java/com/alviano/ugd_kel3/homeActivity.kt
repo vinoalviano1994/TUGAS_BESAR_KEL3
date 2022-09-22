146b628784c111d5ae5e22f786efe28196d7e3db
Dyonisius Alviano M E <vinoalviano1994@gmail.com>
2022-09-22 10:21:36 +0700

    Update Room, RVCustomer.xml dan RVCustomerAdapter.kt (belum selesai)

--- app/src/main/java/com/alviano/ugd_kel3/homeActivity.kt
+++ app/src/main/java/com/alviano/ugd_kel3/homeActivity.kt
@@ -15,11 +15,35 @@ class homeActivity : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val pesananFragment = PesananFragment()
     private val costumerFragment = Fragment_customer()
+    private val accountFragment = AccountFragment()
+    private val homeFragment = HomeFragment()
+    private val pesananFragment = PesananFragment()
+    private val costumerFragment = Fragment_customer()
+
+    lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         changeFragment(costumerFragment)
+        changeFragment(costumerFragment)
+        bottomNav = findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnNavigationItemReselectedListener{
+            when (it.itemId){
+                R.id.menu_home -> {
+                    changeFragment(homeFragment)
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.menu_Order -> {
+                    changeFragment(costumerFragment)
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.menu_account -> {
+
+                }
+            }
+        }
+
 
     }
 
@@ -50,5 +74,13 @@ class homeActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = MenuInflater(this)
+        menuInflater.inflate(R.menu.bottom_navigation, menu)
+        return true
+    }
+
+
 
 }
