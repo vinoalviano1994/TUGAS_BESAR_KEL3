8955a12375633b3c61854768314b24d0858cf34c
vinoalviano1994 <vinoalviano1994@gmail.com>
2022-10-02 12:19:26 +0700

    tambah Firebase + local notif

--- app/src/main/java/com/alviano/ugd_kel3/Registrasi.kt
+++ app/src/main/java/com/alviano/ugd_kel3/Registrasi.kt
@@ -1,12 +1,21 @@
 package com.alviano.ugd_kel3
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.alviano.ugd_kel3.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -17,6 +26,11 @@ class Registrasi : AppCompatActivity() {
     private lateinit var inputNoTelp: TextInputEditText
     private lateinit var inputEmail: TextInputEditText
     private lateinit var mainLayout: ConstraintLayout
+    private val binding: ActivityMainBinding? = null
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val CHANNEL_ID_2 = "channel_notification_02"
+    private val notificationId1 = 101
+    private val notificationId2 = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,27 +54,59 @@ class Registrasi : AppCompatActivity() {
             var noTelp: String = inputNoTelp.getText().toString()
             var email: String = inputEmail.getText().toString()
 
-            if(username.isEmpty()){
+            if (username.isEmpty()) {
                 inputUsername.setError("Username must be filled with text")
                 checkRegistrasi = false
             }
-            if(password.isEmpty()){
+            if (password.isEmpty()) {
                 inputPassword.setError("Password must be filled with text")
                 checkRegistrasi = false
             }
-            if(tanggalLahir.isEmpty()){
+            if (tanggalLahir.isEmpty()) {
                 inputTl.setError("Date of birth must be filled with text")
                 checkRegistrasi = false
             }
-            if(noTelp.isEmpty()){
+            if (noTelp.isEmpty()) {
                 inputNoTelp.setError("Phone number must be filled with text")
                 checkRegistrasi = false
             }
-            if(email.isEmpty()) {
+            if (email.isEmpty()) {
                 inputEmail.setError("Email must be filled with text")
                 checkRegistrasi = false
             }
-            if(!checkRegistrasi) return@OnClickListener
+            if (!checkRegistrasi) return@OnClickListener
+
+            val intent: Intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            val broadcastIntent: Intent = Intent(this, NotificationReceiver::class.java)
+            broadcastIntent.putExtra("toastMessage", "Regsitrasi")
+
+            val actionIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                broadcastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID_1)
+                .setSmallIcon(R.drawable.ic_baseline_looks_one_24)
+                .setContentTitle("Regiter Atma Salon")
+                .setContentText("Anda berhasil Register, silahkan check email untuk verivikasi")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(notificationId1, builder.build())
+            }
+
             val moveLogin = Intent(this@Registrasi, MainActivity::class.java)
             startActivity(moveLogin)
 
@@ -69,5 +115,6 @@ class Registrasi : AppCompatActivity() {
             val moveLogin = Intent(this@Registrasi, MainActivity::class.java)
             startActivity(moveLogin)
         })
+
     }
 }
